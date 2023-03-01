cdb58381ea4dcf5f5847f32ae18c383fa1e27868
Your Name <you@example.com>
2023-03-01 13:34:36 +0530

    Sorting an array using insertion sort

---
+++ AlgorithmAndDataStructurePrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlgorithmAndDataStructurePrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            InsertionSort insertion = new InsertionSort();
+            insertion.Display();
+        }
+    }
+
+}
