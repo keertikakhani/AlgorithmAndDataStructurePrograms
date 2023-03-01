2d86424b13235a1592155c11f27efa37871462d6
Your Name <you@example.com>
2023-03-01 13:52:43 +0530

    Sorting the array using Bubble Sort

---
+++ AlgorithmAndDataStructurePrograms/BubbleSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmAndDataStructurePrograms
+{
+    public class BubbleSort
+    {
+        public void DisplayNumbers()
+        {
+            int[] arr = { 78, 55, 45, 98, 13 };
+            int temp;
+
+            Console.WriteLine("Before Sorting");
+            for (int k = 0; k <arr.Length; k++)
+            {
+                Console.WriteLine(arr[k]);
+            }
+            for (int j = 0; j <= arr.Length-2; j++)
+            {
+                for (int i = 0; i <= arr.Length-2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("After Sorting");
+            foreach (int p in arr)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+    }
+}
--- AlgorithmAndDataStructurePrograms/Program.cs
+++ AlgorithmAndDataStructurePrograms/Program.cs
@@ -6,8 +6,12 @@ namespace AlgorithmAndDataStructurePrograms
     {
         public static void Main(string[] args)
         {
+            
             InsertionSort insertion = new InsertionSort();
             insertion.Display();
+
+            BubbleSort bubble = new BubbleSort();
+            bubble.DisplayNumbers();
         }
     }
 
